36de494bf23997b4a1f17a25ffc488847e41523b
Serge KURANDA <skuranda@iMac-de-Serge.local>
2017-11-12 00:36:54 +0100

    formatage

--- src/components/template/PageHeader.js
+++ src/components/template/PageHeader.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 import {Sidebar, Header} from 'semantic-ui-react'
 import styled from 'styled-components';
 
-const PageHeaderStyled = styled.div`
+const PageHeaderStyled = styled.div `
     padding: 15px;
     border-bottom: 1px solid #dce0e6;
     border-top: 1px solid #eee;
@@ -13,20 +13,19 @@ const PageHeaderStyled = styled.div`
     font-family: 'Montserrat';
 `;
 
-const TitleStyled = styled.span`
+const TitleStyled = styled.span `
     font-size: 16px;
 `;
 
-const DescriptionStyled = styled.span`
+const DescriptionStyled = styled.span `
     display: block;
     font-size: 12px;
     margin-top: 5px;
 `;
 
-const PageHeader = ({title, description}) =>
-    <PageHeaderStyled>
-        <TitleStyled>{title}</TitleStyled>
-        <DescriptionStyled>{description}</DescriptionStyled>
-    </PageHeaderStyled>
+const PageHeader = ({title, description}) => <PageHeaderStyled>
+    <TitleStyled>{title}</TitleStyled>
+    <DescriptionStyled>{description}</DescriptionStyled>
+</PageHeaderStyled>
 
 export default PageHeader;
